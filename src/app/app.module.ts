8608733105ebeceeeef2fe2e9eddea5ebf26d5b1
CJAlvarez <cristhianalvarez96@gmail.com>
2019-08-02 14:51:02 -0600

    Friday

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {Routes, RouterModule } from '@angular/router';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+
+// Servicios
+import { ProveedoresService } from './servicios/proveedores.service';
+
+// Modulos
+
+// Routing
+const routes: Routes = [
+{path: '', component: InicioComponent},
+{path:'proveedores', component: ProveedoresComponent},
+{path:'addprovee', component: AddproveeComponent},
+{path:'**', component: NotFoundComponent}
+];
+
+@NgModule({
+	declarations: [
+	AppComponent,
+	ProveedoresComponent,
+	InicioComponent,
+	NotFoundComponent,
+	HeaderComponent,
+	AddproveeComponent
+	],
+	imports: [
+	BrowserModule,
+	RouterModule.forRoot(routes),
+	FormsModule
+	],
+	providers: [
+	ProveedoresService
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
